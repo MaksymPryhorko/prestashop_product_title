65a51ef0f6a4787abdfdbafad3aedd367b2cf14e
MaksymPryhorko <maxik2007@ukr.net>
2024-01-24 13:06:57 +0200

    Change radioButton to formSelect.
    Change radioButtons to formSelect(dropDown) + refactoring code.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import image from "./img/smallImage.jpg";
 import { useState } from "react";
 
-const loc = {
+const locationPriceClassName = {
   topLeft: "Price-location__top-left",
   topRight: "Price-location__top-right",
   bottomLeft: "Price-location__bottom-left",
@@ -10,112 +10,56 @@ const loc = {
 };
 
 function App() {
-  const [locationPrice, setLocationPrice] = useState("top-left");
-  const [locationPricePx, setLocationPricePx] = useState("Price-location__top-left");
+  const [locationPrice, setLocationPrice] = useState("topLeft");
 
-  function findLocation(locationPrice) {
-    switch (locationPrice) {
-      case "top-left":
-        setLocationPricePx(loc.topLeft);
-        break;
-      case "top-right":
-        setLocationPricePx(loc.topRight);
-        break;
-      case "bottom-left":
-        setLocationPricePx(loc.bottomLeft);
-        break;
-      case "bottom-right":
-        setLocationPricePx(loc.bottomRight);
-        break;
-
-      default:
-        break;
-    }
+  function findLocationPriceClassName(locationPrice) {
+    return locationPriceClassName[locationPrice];
   }
 
-  function onChange(e) {
+  function onChangeLocationPrice(e) {
     setLocationPrice(e.target.value);
-    findLocation(e.target.value);
   }
 
   return (
-    <div className="App">
-      <div style={{ display: "flex" }}>
-        <div style={{ position: "relative" }}>
-          <img
-            style={{ display: "block", width: "290px", height: "430px" }}
-            src={image}
-            alt="Sorry. No foto."
-          />
-          <div
-            className={locationPricePx}
+    <div style={{ display: "flex" }}>
+      <div style={{ position: "relative" }}>
+        <img
+          style={{ display: "block", width: "290px", height: "430px" }}
+          src={image}
+          alt="Sorry. No foto."
+        />
+        <div
+          className={findLocationPriceClassName(locationPrice)}
+          style={{
+            width: "40px",
+            height: "40px",
+            backgroundColor: "red",
+            borderRadius: "20px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            position: "absolute",
+          }}
+        >
+          <p
             style={{
-              width: "40px",
-              height: "40px",
-              backgroundColor: "red",
-              borderRadius: "20px",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              position: "absolute",
+              margin: "0px",
             }}
           >
-            <p
-              style={{
-                margin: "0px",
-              }}
-            >
-              Price
-            </p>
-          </div>
+            Price
+          </p>
         </div>
-
-        <form>
-          <p>Choose a location price:</p>
-          <div style={{ display: "flex", flexDirection: "column", alignItems: "start" }}>
-            <label>
-              <input
-                type="radio"
-                name="locationPrice"
-                value="top-left"
-                onChange={onChange}
-                checked={locationPrice === "top-left"}
-              />
-              Top-left
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="locationPrice"
-                value="top-right"
-                onChange={onChange}
-                checked={locationPrice === "top-right"}
-              />
-              Top-right
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="locationPrice"
-                value="bottom-left"
-                onChange={onChange}
-                checked={locationPrice === "bottom-left"}
-              />
-              Bottom-left
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="locationPrice"
-                value="bottom-right"
-                onChange={onChange}
-                checked={locationPrice === "bottom-right"}
-              />
-              Bottom-right
-            </label>
-          </div>
-        </form>
       </div>
+
+      <form>
+        <p>Choose a location price:</p>
+        <select name="location" onChange={onChangeLocationPrice}>
+          <option value="topLeft">Top-left</option>
+          <option value="topRight">Top-right</option>
+          <option value="bottomLeft">Bottom-left</option>
+          <option value="bottomRight">Bottom-right</option>
+        </select>
+      </form>
     </div>
   );
 }
