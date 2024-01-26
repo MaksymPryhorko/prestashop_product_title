71f6589c715c3ea3431dbbbcda768856783f650e
MaksymPryhorko <maxik2007@ukr.net>
2024-01-26 12:52:24 +0200

    Change my className to Denis className

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import image from "./img/smallImage.jpg";
 import { useState } from "react";
 
 const locationPriceClassName = {
-  topLeft: "Price-location__top-left",
-  topRight: "Price-location__top-right",
-  bottomLeft: "Price-location__bottom-left",
-  bottomRight: "Price-location__bottom-right",
+  0: "Price-location__disabled",
+  1: "Price-location__top-left",
+  2: "Price-location__top-right",
+  3: "Price-location__center-left",
+  4: "Price-location__center-right",
+  5: "Price-location__bottom-left",
+  6: "Price-location__bottom-right",
 };
 
 function App() {
@@ -35,7 +38,6 @@ function App() {
             height: "40px",
             backgroundColor: "red",
             borderRadius: "20px",
-            display: "flex",
             alignItems: "center",
             justifyContent: "center",
             position: "absolute",
@@ -51,15 +53,29 @@ function App() {
         </div>
       </div>
 
-      <form>
+      <div className="col-lg-8">
         <p>Choose a location price:</p>
-        <select name="location" onChange={onChangeLocationPrice}>
-          <option value="topLeft">Top-left</option>
+        <select
+          name="DNKSETCARTRULEWITHLINK_SHOW_STICKER_PRODUCT"
+          className=" fixed-width-xl"
+          id="DNKSETCARTRULEWITHLINK_SHOW_STICKER_PRODUCT"
+          onChange={onChangeLocationPrice}
+        >
+          {/* <option value="topLeft">Top-left</option>
           <option value="topRight">Top-right</option>
           <option value="bottomLeft">Bottom-left</option>
-          <option value="bottomRight">Bottom-right</option>
+          <option value="bottomRight">Bottom-right</option> */}
+          <option value="0">Disabled</option>
+          <option value="1">Top Left</option>
+          <option value="2" selected="selected">
+            Top Right
+          </option>
+          <option value="3">Center Left</option>
+          <option value="4">Center Right</option>
+          <option value="5">Bottom Left</option>
+          <option value="6">Bottom Right</option>
         </select>
-      </form>
+      </div>
     </div>
   );
 }
